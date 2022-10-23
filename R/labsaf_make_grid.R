5d1093fe7df033432cbf22079281ca857c325ba7
ambarja <antony.barja8@gmail.com>
2022-10-23 18:11:33 -0500

    updated labsaf

--- R/labsaf_make_grid.R
+++ R/labsaf_make_grid.R
@@ -4,12 +4,11 @@
 #' @param x, is a sf object (polygon) that represent the study area.
 #' @param size, is the size of the pixel grid.
 #' @return  a sf object (polygons).
-#' @importFrom  sf st_make_grid st_geometry
 #' @export
 labsaf_make_grid <- function(x, size){
-  grid <- st_make_grid(x, size, what = "polygons", square = TRUE)
+  grid <- sf::st_make_grid(x, size, what = "polygons", square = TRUE)
   id_grid <- data.frame(ID = 1:length(grid))
-  st_geometry(id_grid) <- grid
+  sf::st_geometry(id_grid) <- grid
   output <- id_grid[x,]
   return(output)
 }
