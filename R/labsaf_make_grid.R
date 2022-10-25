7dc84c8210b9c9ae280ab17807807c5836f36c10
ambarja <antony.barja8@gmail.com>
2022-10-25 16:47:55 -0500

    updated documentation

--- R/labsaf_make_grid.R
+++ R/labsaf_make_grid.R
@@ -1,12 +1,24 @@
 #' labsaf make grid
+#'
 #' @description
-#' Is a function to create a grid of study area.
-#' @param x, is a sf object (polygon) that represent the study area.
-#' @param size, is the size of the pixel grid.
-#' @return  a sf object (polygons).
+#' `r lifecycle::badge('experimental')`
+#'
+#' This is a function to create a grid regular o hexagonal of the study area.
+#' @param x, is as sf object of type polygon (*UTM project*) that represent the study area.
+#' @param size, is the size of the pixel grid in **meters**.
+#' @param square, is logical; if `FALSE`, create **hexagonal grid**.
+#' @param ..., further arguments of `st_make_grid()` of the sf package.
+#' @return  An sf object of type polygons.
 #' @export
-labsaf_make_grid <- function(x, size){
-  grid <- sf::st_make_grid(x, size, what = "polygons", square = TRUE)
+
+labsaf_make_grid <- function(x, size, square = TRUE, ...){
+  grid <- sf::st_make_grid(
+    x,
+    size,
+    what = "polygons",
+    square = TRUE,
+    ...
+    )
   id_grid <- data.frame(ID = 1:length(grid))
   sf::st_geometry(id_grid) <- grid
   output <- id_grid[x,]
