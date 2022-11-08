ca539a9b90348c295ca0448a09150f20591aabb3
ambarja <antony.barja8@gmail.com>
2022-11-08 10:34:39 -0500

    updated

--- R/labsaf_aedplot.R
+++ R/labsaf_aedplot.R
@@ -6,27 +6,42 @@
 #' This is a function to create a some plot of analysis exploratory of data.
 #' @param data, is an sf or data.frame object with the values of the **soil properties**.
 #' @param var, represent the values of the soil properties.
+#' @param mode, is the output mode static o view plot.
 #' @param fill, is a fill color hexadecimal for plot.
 #' @import patchwork
+#' @importFrom rlang .data
 #' @return  A graphic of ggplot2.
 #' @export
 
-labsaf_eadplot <- function(data, var, fill){
+labsaf_eadplot <- function(data, var, fill, mode = "plot"){
   plot_hist <- ggplot2::ggplot(
     data = data,
-    ggplot2::aes(x = data[[var]])) +
+    ggplot2::aes(x = .data[[var]])) +
     ggplot2::geom_histogram(fill = "#69782e",bins = 9,alpha=0.7) +
-    ggplot2::theme_minimal() +
-    ggplot2::labs(title = sprintf("Histogram of %s",var),x="",y="")
+    ggplot2::theme_minimal()
 
   plot_boxplot <- ggplot2::ggplot(
     data = data,
-    ggplot2::aes(x=var,y = data[[var]])
-  ) +
-    ggplot2::geom_violin(fill = "#69782e", alpha=0.7) +
+    ggplot2::aes(x=var,y = .data[[var]])) +
+    ggplot2::geom_boxplot(fill = "#69782e", alpha=0.7) +
     ggplot2::geom_jitter() +
-    ggplot2::theme_minimal() +
-    ggplot2::labs(title = sprintf("Violin plot of %s",var),x="",y="")
-  panel_plot <- plot_hist | plot_boxplot
+    ggplot2::theme_minimal()
+
+  if(mode == "plot"){
+    plot_hist <- plot_hist +
+      ggplot2::labs(title = sprintf("Histogram of %s",var),x="",y="")
+    plot_boxplot <- plot_boxplot +
+      ggplot2::labs(title = sprintf("Violin plot of %s",var),x="",y="")
+    panel_plot <- plot_hist | plot_boxplot
+  } else{
+    plot_hist <- plot_hist %>% plotly::ggplotly()
+    plot_boxplot <- plot_boxplot %>% plotly::ggplotly()
+    panel_plot <- plotly::subplot(plot_hist,plot_boxplot) %>%
+      plotly::layout(
+        annotations = list(
+          list(x = 0.2 , y = 1.05, text = sprintf("Histogram of %s",var), showarrow = F, xref='paper', yref='paper'),
+          list(x = 0.8 , y = 1.05, text = sprintf("Violin plot of %s",var), showarrow = F, xref='paper', yref='paper'))
+        )
+  }
   return(panel_plot)
 }
--- R/labsaf_geoplot.R
+++ R/labsaf_geoplot.R
@@ -7,26 +7,75 @@
 #' @param region, is an sf object of type polygon that represent the study area.
 #' @param data, is an sf object with the values of the **soil properties**.
 #' @param var, represent the values of the soil properties.
+#' @param mode, is the output mode static o view plot.
 #' @param ..., further arguments of `labs` or `anotation_*` of the ggplot2 and ggspatial package.
+#' @importFrom rlang .data
 #' @return  A graphic of ggplot2.
 #' @export
 
-labsaf_geoplot <- function(
-    region, data, var,...){
-  geomap <- ggplot2::ggplot() +
-    ggplot2::geom_sf(
-      data = region,
-      fill = "white",
-      color = "black"
-    ) +
-    ggplot2::geom_sf(
-      data = data,
-      size = 3,
-      shape = 16,
-      ggplot2::aes(color = data[[var]])
-    ) +
-    ggplot2::labs(color = var,...) +
-    ggspatial::annotation_north_arrow(location="tl",...) +
-    ggspatial::annotation_scale(location="br",...)
+labsaf_geoplot <- function(region, data, var, mode,...){
+  if(mode=="plot"){
+    geomap <- ggplot2::ggplot() +
+      ggplot2::geom_sf(
+        data = region,
+        fill = "white",
+        color = "black"
+      ) +
+      ggplot2::geom_sf(
+        data = data,
+        size = 3,
+        shape = 16,
+        ggplot2::aes(color = data[[var]])
+      ) +
+      ggplot2::labs(color = var,...) +
+      ggspatial::annotation_north_arrow(location="tl",...) +
+      ggspatial::annotation_scale(location="br",...)
+
+  }else{
+    pal <- leaflet::colorNumeric(palette = "viridis",domain = data[[var]])
+    geoplot <- leaflet::leaflet(data = region) %>%
+      leaflet::addProviderTiles(
+        leaflet::providers$OpenStreetMap,
+        group = "OpenStreetMap") %>%
+      leaflet::addProviderTiles(
+        leaflet::providers$CartoDB.Positron,
+        group = "CartoDB.Positron") %>%
+      leaflet::addProviderTiles(
+        leaflet::providers$CartoDB.DarkMatter,
+        group = "CartoDB.DarkMatter") %>%
+      leaflet::addProviderTiles(
+        leaflet::providers$Esri.WorldImagery,
+        group = "ESRI.Satelital") %>%
+      leaflet::addPolygons(
+        color = "black",
+        weight = 1,
+        opacity = 1,
+        fill = NA,
+        group = "region") %>%
+      leaflet::addCircleMarkers(
+        data = data,
+        weight = 0,
+        radius = 5,stroke = 5,
+        fillColor = ~pal(data[[var]]),
+        fillOpacity = 0.8,
+        label = data$PH,
+        group = var
+      ) %>%
+      leaflet::addLegend(
+        data = data,
+        pal = pal,
+        opacity = 1,
+        values = data[[var]],
+        title = var) %>%
+      leaflet::addLayersControl(
+        overlayGroups = c(var,"region"),
+        baseGroups = c(
+          "OpenStreetMap",
+          "CartoDB.Positron",
+          "CartoDB.DarkMatter",
+          "ESRI.Satelital"),
+        position = "topleft"
+      )
+  }
   return(geomap)
 }
